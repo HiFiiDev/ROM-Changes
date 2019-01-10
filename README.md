91ee5b3040fcc5e2172cac038750bf5b00bb4639
Kyler <mbqsniper@hotmail.com>
2019-01-10 07:07:45 -0700

    First commit. WIP

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ROM-Changes
 an easy and customizable way to create changelogs for your ROMs.
+
+WIP
---
+++ changes.sh
@@ -0,0 +1,213 @@
+#!/bin/bash
+# Move this file to your ROM/build !!!
+
+# To run this, simply cd to your ROM folder and run:
+# . build/changes.sh
+# :)
+TodaysDate=$(date +"%m-%d-%Y")
+ChangelogFile=changes/$date/$romname-Changelog-$date.txt
+Changelogger=repo forall -pc git log --reverse --no-merges --since=$NumberOf.days.ago > $ChangelogFile
+
+warmWelcome () {
+    echo "############################################################"
+    echo "#                                                          #"
+    echo "#                                                          #"
+    echo "#                   Changelog creater                      #"
+    echo "#                                                          #"
+    echo "#                                                          #"
+    echo "############################################################"
+    echo " "
+    echo " "
+}
+
+noMoreBoringStuff ()
+{
+    echo " "
+    echo "Good news! We'll never have to do this again. :)"
+    echo "(Unless you delete the folder...)"
+
+    clear
+}
+
+changelogFolder ()
+{
+    # clear
+
+    if [ ! -d changes ]
+    then echo " "
+        echo "Creating Changelog folder..."
+        sleep 1
+        mkdir changes
+        sleep 2
+        echo " "
+        echo "Done!"
+        sleep 1.3
+        echo " "
+
+        # Let the user know what's going on
+        echo "Adding Read and Write permissions to the folder..."
+        sleep 1.6
+        chmod 777 -R Changelogs
+        echo "Done!"
+        echo "Moving on..."
+        sleep 1.5
+
+        # Now that all the boring setup stuff is done, let's let the user know.
+        noMoreBoringStuff
+
+    else echo "Found the Changelog folder!"
+        sleep 1.5
+        echo " "
+        echo "Moving on..."
+        sleep 1.5
+        echo " "
+        echo " "
+
+    fi
+}
+
+setROMName () {
+    if [ ! -f "changelog_config.dat" ] ; then
+        value=$ROMname
+
+        # otherwise read the value from the file
+    else
+        value=`cat changelog_config.dat`
+    fi
+
+    # show it to the user
+    echo "ROM name: ${value}"
+
+    # and save it for next time
+    echo "${value}" > changelog_config.dat
+
+    if [ -z "$value" ]
+    then
+        read -p "Set ROM name: " ROMname
+        echo "Thanks, ROM name is $ROMname"
+        echo " "
+        sleep 2
+    else
+        echo "$value"
+    fi
+
+}
+
+gitChangelog ()
+{
+    echo " "
+    echo "Time to set how far back you'd like the changelog to go."
+    echo " "
+    sleep 1.5
+    echo "How many days back would you like to go?"
+    echo "(enter the word 'today' if you'd like to pull the changes from today only)"
+    echo " "
+    #    echo "** Friendly tip! **"
+    #    echo "It's recommended to repo sync prior to pulling any changelogs"
+    #    echo " "
+    read -p "Amount of days: " NumberOf
+
+    if [ $NumberOf = $NumberOf ]
+    then echo " "
+        echo "Creating directory for todays date..."
+        sleep 2
+        mkdir Changelogs/$(date +"%m-%d-%Y")
+
+        echo " "
+        echo "Entering the directory for $TodaysDate"
+        sleep 1.5
+
+        cd Changelogs/$(date +"%m-%d-%Y")
+
+        echo " "
+        echo "Pulling the changelog from $NumberOf days ago"
+
+        repo forall -pc git log --reverse --no-merges --since=$NumberOf.days.ago > ROM-Changelog-$(date +"%m-%d-%Y").txt
+        echo " "
+        echo "Done!"
+        sleep 2
+
+        cd ..
+        cd ..
+
+        echo " "
+        echo "Settings some permissions..."
+        sleep 1.5
+
+        chmod 777 -R Changelogs
+        echo "Done!"
+        cd ..
+        #cd Changelogs &&
+        # mkdir $Date
+        # cd Changelogs
+        # cd $Date
+        echo " "
+        # echo "Done!"
+        sleep 2.4
+
+        echo " "
+        echo "Go ahead and head on over to your Changelogs/$TodaysDate folder to find the changelog."
+        sleep 3
+        echo " "
+
+        echo "Exiting..."
+        sleep 2
+
+elif [ $NumberOf = "today" ]
+    then echo " "
+        echo "Creating directory for todays date..."
+        sleep 2
+        mkdir Changelogs/$(date +"%m-%d-%Y")
+
+        echo " "
+        echo "Entering the directory for $TodaysDate"
+        sleep 1.5
+
+        cd Changelogs/$(date +"%m-%d-%Y")
+
+        echo " "
+        echo "Pulling the changelog from 1 day ago"
+
+        repo forall -pc git log --reverse --no-merges --since=1.day.ago > ROM-Changelog-$(date +"%m-%d-%Y").txt
+        echo " "
+        echo "Done!"
+        sleep 2
+
+        cd ..
+        cd ..
+
+        echo "Settings some permissions..."
+        sleep 1.5
+
+        chmod 777 -R Changelogs
+        cd ..
+        #cd Changelogs &&
+        # mkdir $Date
+        # cd Changelogs
+        # cd $Date
+        echo " "
+        echo "Done!"
+        sleep 1.5
+
+        echo " "
+        echo "Go ahead and head on over to your Changelogs/$TodaysDate folder to find the changelog."
+        sleep 1.5
+        echo " "
+
+        echo "Exiting..."
+    fi
+
+}
+
+cd build
+
+warmWelcome
+
+# Create the Changelog folder if we can't find it
+changelogFolder
+
+# Set the ROM name for the changelog output
+setROMName
+
+# Time to get to the good stuff
+gitChangelog
