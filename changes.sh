68a05d5bad3fbb471475044d76066e26b7107b1b
Kyler <46557883+HiFiiDev@users.noreply.github.com>
2019-06-02 12:14:42 -0700

    Update changes.sh

--- changes.sh
+++ changes.sh
@@ -45,28 +45,28 @@ warmWelcomeTest () {
     echo
     for l in W e l c o m e ; do
         echo -n ${bold}$l
-        sleep 0.7
+        sleep .50
     done
     echo -n " "
-    sleep 0.7
+    sleep .50
     for l in t o ; do
           echo -n ${bold}$l
-        sleep 0.7
+        sleep .50
     done
     echo -n " "
-    sleep 0.7
+    sleep .50
     for l in H i f i i s ; do
           echo -n ${bold}$l
-        sleep 0.7
+        sleep .50
     done
       echo -n ${bold}$l
-    sleep 0.7
+    sleep .50
     for l in C h a n g e l o g ; do
           echo -n ${bold}$l
-        sleep 0.7
+        sleep .50
     done
     echo -n " "
-    sleep 0.7
+    sleep .50
     for l in C r e a t o r ; do
           echo -n ${bold}$l
         sleep .1
